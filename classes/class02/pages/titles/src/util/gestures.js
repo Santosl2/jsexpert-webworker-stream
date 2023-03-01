027b2ed28a9de89b335908442f48d5e7fdf83f6e
Matheus Filype <mfilype2019@gmail.com>
2023-03-01 10:09:59 -0300

    feat: added copy of class two and renamed to class03

--- classes/class02/pages/titles/src/util/gestures.js
+++ classes/class02/pages/titles/src/util/gestures.js
@@ -1,44 +1,33 @@
-const { GestureDescription, Finger, FingerCurl } = window.fp
+const { GestureDescription, Finger, FingerCurl } = window.fp;
 
-const ScrollDownGesture = new GestureDescription('scroll-down') // ✊️
-const ScrollUpGesture = new GestureDescription('scroll-up') // 🖐
+const ScrollDownGesture = new GestureDescription("scroll-down"); // ✊️
+const ScrollUpGesture = new GestureDescription("scroll-up"); // 🖐
 
-
-// Scroll Down
-// -----------------------------------------------------------------------------
-
+//#region Scroll Down
 // thumb: half curled
 // accept no curl with a bit lower confidence
-ScrollDownGesture.addCurl(Finger.Thumb, FingerCurl.HalfCurl, 1.0)
-ScrollDownGesture.addCurl(Finger.Thumb, FingerCurl.NoCurl, 0.5)
+ScrollDownGesture.addCurl(Finger.Thumb, FingerCurl.HalfCurl, 1.0);
+ScrollDownGesture.addCurl(Finger.Thumb, FingerCurl.NoCurl, 0.5);
+//#endregion Scroll Down
 
 // all other fingers: curled
 for (let finger of [Finger.Index, Finger.Middle, Finger.Ring, Finger.Pinky]) {
-  ScrollDownGesture.addCurl(finger, FingerCurl.FullCurl, 1.0)
-  ScrollDownGesture.addCurl(finger, FingerCurl.HalfCurl, 0.9)
+  ScrollDownGesture.addCurl(finger, FingerCurl.FullCurl, 1.0);
+  ScrollDownGesture.addCurl(finger, FingerCurl.HalfCurl, 0.9);
 }
 
-
-// ScrollUp
-// -----------------------------------------------------------------------------
-
+//#region Scroll Up
 // no finger should be curled
 for (let finger of Finger.all) {
-  ScrollUpGesture.addCurl(finger, FingerCurl.NoCurl, 1.0)
+  ScrollUpGesture.addCurl(finger, FingerCurl.NoCurl, 1.0);
 }
+//#endregion Scroll Up
 
-
-const knownGestures = [
-  ScrollDownGesture,
-  ScrollUpGesture,
-]
+const knownGestures = [ScrollDownGesture, ScrollUpGesture];
 
 const gestureStrings = {
-  'scroll-up': '🖐',
-  'scroll-down': '✊️'
-}
+  "scroll-up": "🖐",
+  "scroll-down": "✊️",
+};
 
-export {
-  knownGestures,
-  gestureStrings
-}
+export { knownGestures, gestureStrings };
